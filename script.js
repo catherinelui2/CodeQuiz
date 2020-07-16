e1cbebe87a7d89d3869b7a621104a22eb3e496f6
SpazCat <catherinelui.2@gmail.com>
2020-07-15 22:28:33 -0600

    added JS variables

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+//variables
+var score = "scores";
+var questions = "questions";
+var answers = "answers";
+var button = "button";
+//DOM variables
+
 // when user clicks start quiz button, it should take them to the array of questions with mutliple choices
 
 // timer is also set to start 60seconds and counting
 
 
-//
+//need if then statements to prompt correct/wrong messages when user answers it wrong
+//Also need the statement to start collecting scores 
+
+
+
+

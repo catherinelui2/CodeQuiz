e1177670d207f4b14b74afeda3fc340bc686dff7
CatL <catherinelui.2@gmail.com>
2020-07-16 23:13:49 -0600

    Added a series of questions, choices, div ids for question/choices containers.

--- script.js
+++ script.js
@@ -1,11 +1,75 @@
 //variables
 var score = "scores";
-var questions = "questions";
-var answers = "answers";
-var button = "button";
-//DOM variables
+var count = 0;
+var body = document.querySelector("body");
+var startButton = document.querySelector(".start-button");
+var header = document.querySelector("header");
+var main = document.querySelector("main");
+var questionBox = document.querySelector("#question-container");
+var choicesBox = document.querySelector("#choices-container");
+//questions arrays:
+var questions = [
+    "Which of the following would you use to for in-line comments?",
+    "What is an index?",
+    "What is a syntax?",
+    "what are variables?",
+    "What are strings?"
+];
 
-// when user clicks start quiz button, it should take them to the array of questions with mutliple choices
+var choices = {
+"choicesOne": [
+"//",
+"\\",
+"[]"
+],
+"choicesTwo": [
+"A special variable, which can hold more than one value at a time",
+"A special character, which can hold one value",
+"A symbol"
+],
+
+"choicesThree": [
+"A defines two types of values: Fixed values and variable values",
+"A value",
+"A number type that define values"
+],
+
+"choicesFour": [
+"Containers for storing data values",
+"Containers for storing html values",
+"Containers for storing css values"
+],
+
+"choicesFive": [
+"Are used for storing and manipulating text",
+"Are used for storing and manipulating numbers",
+"Are used for storing and manipulating integers"
+]
+
+}
+
+
+
+
+//loops 
+//question loop
+function fireQuestions (){
+    for (var i = 0; 0<questions.length; i++) {
+        body.appendChild(buttonEl);
+        buttonEl.textContent = questions[i];
+    }
+}
+
+
+// when user clicks start quiz button, clear out existing text in the main and header tag. 
+startButton.addEventListener("click", function() {
+    main.style.display = "none";
+    header.style.display = "none";
+
+});
+
+
+// the array of questions with mutliple choices pops up
 
 // timer is also set to start 60seconds and counting
 

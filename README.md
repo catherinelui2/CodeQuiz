cbeabb57d92104bbb641f39da4c164b57c278f48
CatL <catherinelui.2@gmail.com>
2020-07-18 01:29:29 -0600

    Updated quote to make functions and buttons

--- README.md
+++ README.md
@@ -3,20 +3,17 @@ Homework 03
 
 Pseudo Code
 
-We need a page to view and store highscores
-
-A button to begin the quiz
-
-A list of questions, display mutliple choices as buttons
-
-Display onclick when users selects an answer
-
-timer on top right corner to decrement fo 60 secs
-
-score keeper
-
-allow user to input name and score to local storage
-
-
-
+- start button when clicked starts quiz
+    -timer starts
+    -first question show
+        multiple choices that allow me to choose from 3
+-user answers question
+    -what to do?
+        -present the next question
+        -if the user answers incorrectly
+            -then subrtract time from timer
+            -and go tothe next question
+-if all the questions have been answered or the timer is 0
+    - then game is over
+-get score, enter initials, save to LS
 
--- script.js
+++ script.js
@@ -1,82 +1,154 @@
+// -if all the questions have been answered or the timer is 0
+//     - then game is over
+// -get score, enter initials, save to LS
+
 //variables
-var score = "scores";
-var count = 0;
-var body = document.querySelector("body");
+const SCORE_BOARD_KEY = "scoreBoard";
+var score = 0;
+var timer = 0;
+var currentQuestionnaireIndex = 0;
+
+var choiceOneEl = document.querySelector("#button1");
+var choiceTwoEl = document.querySelector("#button2");
+var choiceThreeEl = document.querySelector("#button3");
+var submitScore = document.querySelector("#submitScore");
+var rightOrWrongEl = document.querySelector("#right-or-wrong");
+var timerEl = document.querySelector("#timer");
 var startButton = document.querySelector(".start-button");
 var header = document.querySelector("header");
 var main = document.querySelector("main");
-var questionBox = document.querySelector("#question-container");
+var questionBox = document.querySelector("#questions-container");
 var choicesBox = document.querySelector("#choices-container");
-//questions arrays:
-var questions = [
-    "Which of the following would you use to for in-line comments?",
-    "What is an index?",
-    "What is a syntax?",
-    "what are variables?",
-    "What are strings?"
+var sectionEl = document.querySelector("section");
+var questionnaires = [
+    {
+        question: "Which of the following would you use to for in-line comments?",
+        answers: [
+            { text: "//", correct: true },
+            { text: "--", correct: false },
+            { text: "[]", correct: false },
+        ],
+    },
+    {
+        question: "What is an index?",
+        answers: [
+            {
+                text: "A special variable, which can hold more than one value at a time",
+                correct: true,
+            },
+            {
+                text: "A special character, which can hold one value",
+                correct: false,
+            },
+            { text: "A symbol", correct: false },
+        ],
+    },
+    {
+        question: "What is a syntax?",
+        answers: [
+            {
+                text: "A defines two types of values: Fixed values and variable values",
+                correct: true,
+            },
+            { text: "A value", correct: false },
+            { text: "A number type that define values", correct: false },
+        ],
+    },
+    {
+        question: "what are variables?",
+        answers: [
+            { text: "Containers for storing data values", correct: true },
+            { text: "Containers for storing html values", correct: false },
+            { text: "Containers for storing css values", correct: false },
+        ],
+    },
+    {
+        question: "What are strings?",
+        answers: [
+            {
+                text: "Are used for storing and manipulating text",
+                correct: true,
+            },
+            {
+                text: "Are used for storing and manipulating numbers",
+                correct: false,
+            },
+            {
+                text: "Are used for storing and manipulating integers",
+                correct: false,
+            },
+        ],
+    },
 ];
 
-var choices = {
-"choicesOne": [
-"//",
-"\\",
-"[]"
-],
-"choicesTwo": [
-"A special variable, which can hold more than one value at a time",
-"A special character, which can hold one value",
-"A symbol"
-],
-
-"choicesThree": [
-"A defines two types of values: Fixed values and variable values",
-"A value",
-"A number type that define values"
-],
-
-"choicesFour": [
-"Containers for storing data values",
-"Containers for storing html values",
-"Containers for storing css values"
-],
-
-"choicesFive": [
-"Are used for storing and manipulating text",
-"Are used for storing and manipulating numbers",
-"Are used for storing and manipulating integers"
-]
-
+//question loop
+//attach this to the click handler for the start button
+function startQuiz() {
+    timer = 80;
+    function myTimer() {
+        timer--;
+        timerEl.textContent = timer;
+    }
+    setInterval(myTimer, 1000);
+    displayQuestionnaire();
 }
 
-
-
-
-//loops 
-//question loop
-function fireQuestions (){
-    for (var i = 0; 0<questions.length; i++) {
-        body.appendChild(buttonEl);
-        buttonEl.textContent = questions[i];
+function displayQuestionnaire() {
+    var questionnaire = questionnaires[currentQuestionnaireIndex];
+    questionBox.innerHTML = questionnaire.question;
+    var answers = questionnaire.answers;
+    choicesBox.innerHTML = "";
+    for (var answer of answers) {
+        var buttonEl = document.createElement("button");
+        buttonEl.classList.add("btn", "btn-secondary");
+        buttonEl.innerText = answer.text;
+        buttonEl.value = answer.correct;
+        choicesBox.appendChild(buttonEl);
+        buttonEl.addEventListener("click", function () {
+            var correct = this.value;
+            if (correct) {
+                rightOrWrongEl.innerText = "That's correct!";
+                score++;
+            } else {
+                rightOrWrongEl.innerText = "That's WRONG!";
+                timer -= 5;
+            }
+            currentQuestionnaireIndex++;
+            if (currentQuestionnaireIndex < questionnaires.length) {
+                displayQuestionnaire();
+            } else {
+                endGame();
+            }
+        });
     }
 }
 
+function endGame() {
+    questionBox.style.display = "none";
+    choicesBox.style.display = "none";
+    submitScore.style.display = "block";
+    var scoreEl = submitScore.querySelector("#score");
+    scoreEl.innerText = "Your Score is: " + score;
+    var scoreButton = submitScore.querySelector("#scoreButton");
+    var nameInput = submitScore.querySelector("#playerName");
+    scoreButton.addEventListener("click", function () {
+        var scoreBoardEntry = { userName: nameInput.value, score: score};
+        var scoreBoardEntries = localStorage.getItem(SCORE_BOARD_KEY);
+        if (scoreBoardEntries) {
+            scoreBoardEntries = JSON.parse(scoreBoardEntry);
+        } else {
+            scoreBoardEntries = [];
+        }
+        scoreBoardEntries.push(scoreBoardEntry);
+        scoreBoardEntries.sort((e1, e2) => e2.score - e1.score);
 
-// when user clicks start quiz button, clear out existing text in the main and header tag. 
-startButton.addEventListener("click", function() {
+        localStorage.setItem(SCORE_BOARD_KEY, JSON.stringify(scoreBoardEntries));
+    });
+}
+
+// when user clicks start quiz button, clear out existing text in the main and header tag.
+startButton.addEventListener("click", function () {
     main.style.display = "none";
     header.style.display = "none";
-
+    startQuiz();
 });
-
-
-// the array of questions with mutliple choices pops up
-
-// timer is also set to start 60seconds and counting
-
-
-//need if then statements to prompt correct/wrong messages when user answers it wrong
-//Also need the statement to start collecting scores 
-
-
-
-
